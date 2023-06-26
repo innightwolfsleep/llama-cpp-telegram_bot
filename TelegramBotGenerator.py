5cc9da932a6a2b5985d47d4273cb42150e7e9bac
innightwolfsleep <innightwolfsleep@yandex.ru>
2023-06-26 21:33:31 +0600

    prepeare to universal llm wrampper
    prepeare to universal llm wrampper

--- TelegramBotGenerator.py
+++ TelegramBotGenerator.py
@@ -1,16 +1,27 @@
-from llama_cpp import Llama
-import os
 
 #  Place where path to LLM file stored
 telegram_llm_model_path_file = "telegram_llm_model_path.txt"
 
+mode = "llama_cpp"
 n_ctx = 8196
 seed = 0
 n_gpu_layers = 0
-#  Get llm_generator
-with open(telegram_llm_model_path_file, "r") as model_path_file:
-    data = model_path_file.read().rstrip()
-    llm_generator: Llama = Llama(model_path=data, n_ctx=n_ctx, seed=seed, n_gpu_layers=n_gpu_layers)
+
+if mode == "llama_cpp":
+    from generators.GeneratorLlamaCpp import get_answer as g_get_answer
+    from generators.GeneratorLlamaCpp import tokens_count as g_tokens_count
+    from generators.GeneratorLlamaCpp import get_model_list as g_get_model_list
+    from generators.GeneratorLlamaCpp import load_model as g_load_model
+elif mode == "LangchainLlama":
+    from generators.GeneratorLangchainLlamaCpp import get_answer as g_get_answer
+    from generators.GeneratorLangchainLlamaCpp import tokens_count as g_tokens_count
+    from generators.GeneratorLangchainLlamaCpp import get_model_list as g_get_model_list
+    from generators.GeneratorLangchainLlamaCpp import load_model as g_load_model
+elif mode == "transformers":
+    from generators.GeneratorTransformers import get_answer as g_get_answer
+    from generators.GeneratorTransformers import tokens_count as g_tokens_count
+    from generators.GeneratorTransformers import get_model_list as g_get_model_list
+    from generators.GeneratorTransformers import load_model as g_load_model
 
 
 def get_answer(
@@ -24,34 +35,21 @@ def get_answer(
     # Preparing, add stopping_strings
     answer = default_answer
 
-    try:
-        answer = llm_generator.create_completion(
-            prompt=prompt,
-            temperature=generation_params["temperature"],
-            top_p=generation_params["top_p"],
-            top_k=generation_params["top_k"],
-            repeat_penalty=generation_params["repetition_penalty"],
-            stop=stopping_strings,
-            max_tokens=generation_params["max_new_tokens"],
-            echo=True)
-        answer = answer["choices"][0]["text"].replace(prompt, "")
-    except Exception as exception:
-        print("generator_wrapper get answer error ", exception)
+    print("mode =", mode)
+    print("stopping_strings =", stopping_strings)
+    print(prompt, end="")
+    answer = g_get_answer(prompt, generation_params, eos_token, stopping_strings, default_answer, turn_template)
+    print(answer)
     return answer
 
 
 def tokens_count(text: str):
-    return len(llm_generator.tokenize(text.encode(encoding="utf-8", errors="strict")))
+    return g_tokens_count(text)
 
 
 def get_model_list():
-    bins = []
-    for i in os.listdir("models"):
-        if i.endswith(".bin"):
-            bins.append(i)
-    return bins
+    return g_get_model_list()
 
 
 def load_model(model_file: str):
-    with open("models\\" + model_file, "r") as model:
-        llm_generator: Llama = Llama(model_path=model.read(), n_ctx=n_ctx, seed=seed)
+    return g_load_model(model_file)
--- readme.md
+++ readme.md
@@ -1,31 +1,31 @@
-#Extension connecting llama-cpp-python to telegram bot api.
+#Extension connecting llm_python to telegram bot api.
 -
 ![Image1](https://github.com/innightwolfsleep/storage/raw/main/textgen_telegram.PNG)
 
-This is wrapper for [abetlen/llama-cpp-python](https://github.com/abetlen/llama-cpp-python) providing chat like telegram bot interface.
+Providing chat like telegram bot interface with [abetlen/llm_python](https://github.com/abetlen/llm_python), [langchain](https://pypi.org/project/langchain/) or transformers (tbc) 
 
 REQUIREMENTS:
 - python-telegram-bot==13.15
 - pyyaml
 - deep-translator==1.9.2
-- llama-cpp-python
+- llm_python
 
 HOW TO INSTALL:
 1) clone this repo to "text-generation-webui\extensions"
 ```
 cd text-generation-webui
-git clone https://github.com/innightwolfsleep/llama-cpp-telegram_bot 
+git clone https://github.com/innightwolfsleep/llm_telegram_bot 
 ```
 2) install requirements. 
 ```
-pip install -r llama-cpp-telegram_bot\requirements.txt
+pip install -r llm_telegram_bot\requirements.txt
 ```
 
 HOW TO USE:
-1) add your bot token to llama-cpp-telegram_bot/telegram_token.txt (ask https://t.me/BotFather how to get token)
-2) add your model bin file to llama-cpp-telegram_bot/models
-3) write path to your bin model file in llama-cpp-telegram_bot/telegram_llm_model_path.txt
-2) run "python llama-cpp-telegram_bot/main.py"
+1) add your bot token to llm_telegram_bot/telegram_token.txt (ask https://t.me/BotFather how to get token)
+2) add your model bin file to llm_telegram_bot/models
+3) write path to your bin model file in llm_telegram_bot/telegram_llm_model_path.txt
+2) run "python llm_telegram_bot/main.py"
 
 FEATURES:
 - chat and notebook modes
@@ -40,12 +40,3 @@ FEATURES:
 - "++" prefix permanently replace bot name during chat (switch conversation to another character)
 - save/load history in chat by downloading/forwarding to chat .json file
 - integrated auto-translate (you can set model/user language parameter) 
-- integrated silero_tts - you can get voice message from bot
-- translation_as_hidden_text option in .cfg - if you want to learn english with bot)))
-
-TBC:
-- replace "X typing" by yield from generator
-- group chat mode (need to be tested, does current workflow is ok?)
-- migrate to aiogram or not?
-- 1-world-delete-button
-- help button?
