d7238200141e832f642ff2c58d1ac2ce038e16bc
innightwolfsleep <innightwolfsleep@yandex.ru>
2023-06-27 23:17:49 +0600

    Add files via upload

--- TelegramBotGenerator.py
+++ TelegramBotGenerator.py
@@ -2,26 +2,21 @@
 #  Place where path to LLM file stored
 telegram_llm_model_path_file = "telegram_llm_model_path.txt"
 
-mode = "llama_cpp"
+mode = "GeneratorLlamaCpp"
 n_ctx = 8196
 seed = 0
 n_gpu_layers = 0
 
-if mode == "llama_cpp":
-    from generators.GeneratorLlamaCpp import get_answer as g_get_answer
-    from generators.GeneratorLlamaCpp import tokens_count as g_tokens_count
-    from generators.GeneratorLlamaCpp import get_model_list as g_get_model_list
-    from generators.GeneratorLlamaCpp import load_model as g_load_model
-elif mode == "LangchainLlama":
-    from generators.GeneratorLangchainLlamaCpp import get_answer as g_get_answer
-    from generators.GeneratorLangchainLlamaCpp import tokens_count as g_tokens_count
-    from generators.GeneratorLangchainLlamaCpp import get_model_list as g_get_model_list
-    from generators.GeneratorLangchainLlamaCpp import load_model as g_load_model
-elif mode == "transformers":
-    from generators.GeneratorTransformers import get_answer as g_get_answer
-    from generators.GeneratorTransformers import tokens_count as g_tokens_count
-    from generators.GeneratorTransformers import get_model_list as g_get_model_list
-    from generators.GeneratorTransformers import load_model as g_load_model
+# import generator
+if mode == "GeneratorLlamaCpp":
+    from generators.GeneratorLlamaCpp import Generator
+    generator = Generator(telegram_llm_model_path_file, n_ctx, seed, n_gpu_layers)
+elif mode == "GeneratorLangchainLlamaCpp":
+    import generators.GeneratorLangchainLlamaCpp as Generator
+    generator = Generator(telegram_llm_model_path_file, n_ctx, seed, n_gpu_layers)
+elif mode == "GeneratorTransformers":
+    import generators.GeneratorTransformers as Generator
+    generator = Generator(telegram_llm_model_path_file, n_ctx, seed, n_gpu_layers)
 
 
 def get_answer(
@@ -38,18 +33,21 @@ def get_answer(
     print("mode =", mode)
     print("stopping_strings =", stopping_strings)
     print(prompt, end="")
-    answer = g_get_answer(prompt, generation_params, eos_token, stopping_strings, default_answer, turn_template)
+    try:
+        answer = generator.get_answer(prompt, generation_params, eos_token, stopping_strings, default_answer, turn_template)
+    except Exception as e:
+        print("generation error:", e)
     print(answer)
     return answer
 
 
 def tokens_count(text: str):
-    return g_tokens_count(text)
+    return generator.tokens_count(text)
 
 
 def get_model_list():
-    return g_get_model_list()
+    return generator.get_model_list()
 
 
 def load_model(model_file: str):
-    return g_load_model(model_file)
+    return generator.load_model(model_file)
